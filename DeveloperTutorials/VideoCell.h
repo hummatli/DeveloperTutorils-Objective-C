80dcc5f3245d6fbf06b3290ee4c3221149f1b8f9
Sattar Hummatli <settarxan@gmail.com>
2017-07-30 17:33:01 +0400

    added Table View and custom cells

---
+++ DeveloperTutorials/VideoCell.h
@@ -0,0 +1,13 @@
+//
+//  VideoCell.h
+//  DeveloperTutorials
+//
+//  Created by Settar Hummetli on 7/30/17.
+//  Copyright © 2017 Settar Hummetli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VideoCell : UITableViewCell
+
+@end
---
+++ DeveloperTutorials/VideoCell.m
@@ -0,0 +1,40 @@
+//
+//  VideoCell.m
+//  DeveloperTutorials
+//
+//  Created by Settar Hummetli on 7/30/17.
+//  Copyright © 2017 Settar Hummetli. All rights reserved.
+//
+
+#import "VideoCell.h"
+
+@interface VideoCell()
+@property (weak, nonatomic) IBOutlet UILabel *titleLbl;
+@property (weak, nonatomic) IBOutlet UILabel *descLbl;
+@property (weak, nonatomic) IBOutlet UIImageView *thumbImg;
+@property (weak, nonatomic) IBOutlet UIView *cellView;
+
+
+@end
+
+@implementation VideoCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+
+    self.layer.cornerRadius = 2.0;
+    self.layer.shadowColor = [UIColor colorWithRed:157.0 / 255.0 green:157.0 / 255.0 blue:157.0 / 255.0 alpha:0.8].CGColor;
+    self.layer.shadowOpacity = 0.8;
+    self.layer.shadowRadius = 5.0;
+    self.layer.shadowOffset = CGSizeMake(0.0, 2.0);
+    
+    
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
